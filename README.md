26b392c03c3a929430969cb7de4105f79f39dea3
hiagoemanuel <hiagoemanuel321@hotnail.com>
2022-10-31 22:51:37 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# fetch-github-api 
+Este projeto foi retirado do curso DevQuest. E teve como objetivo praticar os conhecimentos de js avançado. 😀
---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
+
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
+
+document.getElementById('btn-search').addEventListener('click', validateInput)
+document.getElementById('input-search').addEventListener('keyup', e => e.key === 'Enter' ? validateInput() : e)
+
+function validateInput() {
+    const userSearch = document.getElementById('input-search').value
+    userSearch.length === 0 ? alert('Preencha o campo com o nome do usuário do GitHub!') : getUserData(userSearch)
+}
+
+async function getUserData(userName) {
+    try {
+        const userResponse = await getUser(userName)
+        const respositoriesResponse = await getRepositories(userName)
+
+        user.setInfo(userResponse)
+        user.setRepositories(respositoriesResponse)
+
+        screen.renderUser(user)
+    } catch {
+        screen.renderNotFound()
+    }
+}
